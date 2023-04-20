4404e4405f5b2ebe44e2008fb53eb713ae5701f1
andlicon <andresjzabalac@hotmail.com>
2023-04-20 18:02:27 -0400

    ADD: generarCartaAleatoria ADD: estilos base para las cartas ADD: HTMl de la solucion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,63 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const CONSTANTES_CARTAS = [
+  {
+    'tipo': 'corazon',
+    'color': 'red',
+    'imagen': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png'
+  },
+  {
+    'tipo': 'trebol',
+    'color': 'black',
+    'imagen' : 'https://cdn-icons-png.flaticon.com/512/105/105219.png'
+  },
+  {
+    'tipo': 'diamante',
+    'color': 'red',
+    'imagen': 'https://images.emojiterra.com/google/android-nougat/512px/2666.png'
+  },
+  {
+    'tipo': 'pica',
+    'color': 'black',
+    'imagen': 'https://images.emojiterra.com/openmoji/v13.1/512px/2660.png'
+  }
+]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const getTipoCarta = (index) => {
+  if(index>=CONSTANTES_CARTAS.length || index<0) {
+    return null;
+  }
+
+  return CONSTANTES_CARTAS[index];
+}
+
+const convertirNumeroCartaValido = (numero) => {
+  
+  if(numero<=0 || numero>12) return null; //valida
+  if(numero==1) return 'A';
+  if(numero==10) return 'J'
+  if(numero==11) return 'Q'
+  if(numero==12) return 'K'
+
+  return numero;
+}
+
+const generarCartaAleatoria = () => {
+  const aleatorioTipoCarta = Math.floor(Math.random() * CONSTANTES_CARTAS.length);
+  const tipoCarta = getTipoCarta(aleatorioTipoCarta);
+  
+  const aleatorioNumeroCarta = Math.floor(Math.random() * 12)+1;
+  const numeroCarta = convertirNumeroCartaValido(aleatorioNumeroCarta);
+
+  if(tipoCarta==null || numeroCarta==null) {
+    return 'ERROR AL GENERAR CARTA';
+  }
+
+  const carta = tipoCarta;
+  carta['numero'] = numeroCarta;
+  return carta;
+}
+
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const carta = generarCartaAleatoria();
 };
