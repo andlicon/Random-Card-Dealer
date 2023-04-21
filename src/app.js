85aa698f938feb9a3842662458b6515323f16582
andlicon <andresjzabalac@hotmail.com>
2023-04-21 08:50:57 -0400

    ADD: se anadio un range para generar x cantidad de cartas ADD: se crea unos numeros que indican la cantidad que el range especifica con js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const TIPO_CARTAS = [
   {
     'nombre': 'trebol',
     'color': 'black',
-    'clase' : 'tipo--trebol'
+    'clase': 'tipo--trebol'
   },
   {
     'nombre': 'diamante',
@@ -22,7 +22,7 @@ const TIPO_CARTAS = [
 ]
 
 const getTipoCarta = (index) => {
-  if(index>=TIPO_CARTAS.length || index<0) {
+  if (index >= TIPO_CARTAS.length || index < 0) {
     return null;
   }
 
@@ -30,12 +30,12 @@ const getTipoCarta = (index) => {
 }
 
 const convertirNumeroCartaValido = (numero) => {
-  
-  if(numero<=0 || numero>12) return null; //valida
-  if(numero==1) return 'A';
-  if(numero==10) return 'J'
-  if(numero==11) return 'Q'
-  if(numero==12) return 'K'
+
+  if (numero <= 0 || numero > 12) return null; //invalido
+  if (numero == 1) return 'A';
+  if (numero == 10) return 'J'
+  if (numero == 11) return 'Q'
+  if (numero == 12) return 'K'
 
   return numero;
 }
@@ -43,11 +43,11 @@ const convertirNumeroCartaValido = (numero) => {
 const generarCartaAleatoria = () => {
   const aleatorioTipoCarta = Math.floor(Math.random() * TIPO_CARTAS.length);
   const tipoCarta = getTipoCarta(aleatorioTipoCarta);
-  
-  const aleatorioNumeroCarta = Math.floor(Math.random() * 12)+1;
+
+  const aleatorioNumeroCarta = Math.floor(Math.random() * 12) + 1;
   const numeroCarta = convertirNumeroCartaValido(aleatorioNumeroCarta);
 
-  if(tipoCarta==null || numeroCarta==null) {
+  if (tipoCarta == null || numeroCarta == null) {
     return 'ERROR AL GENERAR CARTA';
   }
 
@@ -63,9 +63,9 @@ const renderCarta = (carta) => {
   divCarta.classList.add('carta');
 
   //Div de los simbolos
-  for(let i=0; i<2; i++) {
+  for (let i = 0; i < 2; i++) {
     const divTipoCarta = document.createElement('div');
-    const posicion = i==0 ? 'tipo-carta--superior' : 'tipo-carta--inferior';
+    const posicion = i == 0 ? 'tipo-carta--superior' : 'tipo-carta--inferior';
     divTipoCarta.classList.add('tipo-carta', carta.clase, posicion);
     divCarta.appendChild(divTipoCarta);
   }
@@ -81,9 +81,47 @@ const renderCarta = (carta) => {
   display.appendChild(divCarta);
 }
 
+const eventoClickGenerador = () => {
+  const displayViejo = document.querySelector('#display-cartas');
+  document.body.removeChild(displayViejo);
 
-window.onload = function() {
-  //write your code here
+  const nuevoDisplay = document.createElement('div');
+  nuevoDisplay.id = 'display-cartas';
+  nuevoDisplay.classList.add('container-carta');
+  document.body.appendChild(nuevoDisplay);
+
   renderCarta(generarCartaAleatoria());
+}
+
+const generarNumerosInputRange = () => {
+  const rangesArray = document.querySelectorAll('input[type="range"]');
+  const indicadoresArray = document.querySelectorAll('input[type="range"] + .range-indicadores');
+
+  for (let i = 0; i < rangesArray.length; i++) {
+    const range = rangesArray[i];
+    const indicador = indicadoresArray[i];
+
+    const inicio = Number(range.min);
+    const cantidad = Number(range.max);
+    const step = Number(range.step);
+
+    for (let j = inicio; j <= cantidad; j += step) {
+      const spanIndicador = document.createElement('span');
+      spanIndicador.classList.add('indicador');
+      spanIndicador.textContent = j;
+      indicador.appendChild(spanIndicador);
+    }
+
+  }
+}
+
+window.onload = function () {
+  //llenar 
+  generarNumerosInputRange();
+
+  renderCarta(generarCartaAleatoria());
+
+  const generador = document.querySelector('#generador');
+  generador.addEventListener('click', eventoClickGenerador);
 
 };
